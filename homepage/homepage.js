2152e878a01bf431557f29613779476ad8b6daed
kmart138 <kmart138@ucsc.edu>
2025-06-08 20:56:01 -0700

    file

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,5 +1,6 @@
+
 $(document).ready(function () {
-  $("#darkModeToggle").click(function () {
-    $("body").toggleClass("dark-mode");
+  $('#lightModeToggle').click(function () {
+    $('body').toggleClass('light-mode');
   });
-});
+})
